69766a61cbee4d755d04288fefcf403bbd4a14ce
Hicmade <kozikowski.bartosz@gmail.com>
2020-12-11 13:28:50 +0100

    Minor corrections

---
+++ Basic_pfs/Assets/Darth_Artisan/Free_Trees/Meshes/Materials/msg_hide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class msg_hide : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        StartCoroutine("HideMsg");   
+    }
+
+    IEnumerator HideMsg(){
+        yield return new WaitForSeconds(3);
+        gameObject.SetActive(false);
+    }
+
+}
--- Basic_pfs/Assets/Scripts/specbuttion.cs
+++ Basic_pfs/Assets/Scripts/specbuttion.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class specbuttion : MonoBehaviour
 {
     public GameObject spec;
+
     void OnTriggerEnter(Collider col){
         if (col.tag == "Player"){
             spec.SetActive(true);
