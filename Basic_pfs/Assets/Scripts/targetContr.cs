adf51b3cb806f083bc56bca00c0f1d1cdfd9b761
Hicmade <kozikowski.bartosz@gmail.com>
2020-12-11 09:32:37 +0100

    Added target reaction

--- Basic_pfs/Assets/Scripts/targetContr.cs
+++ Basic_pfs/Assets/Scripts/targetContr.cs
@@ -6,6 +6,8 @@ public class targetContr : MonoBehaviour
 {
     [Range(0,2)]
     public int enemy_type;
+    public Material mat1;
+    public Material mat2;
 
     public void Hit_target(Vector3 force_dir, float force_strength){
         GetComponent<Rigidbody>().AddForce(force_dir*force_strength);
@@ -17,6 +19,8 @@ public class targetContr : MonoBehaviour
     }
 
     IEnumerator Destroy_target(){
+        GetComponent<AudioSource>().Play();
+        GetComponent<MeshRenderer>().material = mat2;
         yield return new WaitForSeconds(3);
         Destroy(gameObject);
     }
