306b983e77c9acf24c01f2d6985044f00c019d5a
Hicmade <kozikowski.bartosz@gmail.com>
2020-12-11 08:55:17 +0100

    Added special items

---
+++ Basic_pfs/Assets/Darth_Artisan/Free_Trees/Meshes/Materials/rexcont.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class rexcont : MonoBehaviour
+{
+    public GameObject fire;
+    public GameObject win_screen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col){
+        fire.SetActive(true);
+        StartCoroutine("santamantra");
+    }
+
+    IEnumerator santamantra(){
+        yield return new WaitForSeconds(2);
+        win_screen.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
